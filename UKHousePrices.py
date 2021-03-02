171ab496a3fe6c45445aed72735a12ef5a1e8851
James Bearne <James.Bearne@baringa.com>
2021-03-02 00:41:39 +0000

    Update UKHousePrices.py
    Working to read in multiple files, save as .CSV, groupby and plot bar chart.

--- UKHousePrices.py
+++ UKHousePrices.py
@@ -3,32 +3,40 @@
 
 Filename: house_prices.py
 Descripton: TBC
-TODO#1: Add loop to go through all files in directory
-"""
-filenames = ["/Users/Phani/Desktop/sales-jan-2015.csv",
-"/Users/Phani/Desktop/sales-feb-2015.csv"]
-dataframes = []
-for f in filenames:
-    dataframes.append(pd.read_csv(f))
-"""
-TODO#2: Change new files to load same columns in order to loop through
+TODOs
+TODO#2: Update the data in PROPERTY_TYPE + PPD_CATEGORY_STATUS column by joining on reference
+TODO#3: Groupby on PROPERTY_TYPE and OLD_NEW as required
+TODO#4: Pass in only part of postcode to widen the number of transactions
+TODO#5: Handle the PPD_CATEGORY + RECORD_STATUS rows correctly
+TODO#6: Plot the price change
 
 """
 
 import pandas as pd
 import os
+import datetime
+import matplotlib.pyplot as plt
 
 # Set input data directory
 input_dir = r'C:\Users\James.Bearne\Desktop\Python_HousePrice\data\input'
 
-#List the input data files in directory and build filepath
-input_files = os.listdir(input_dir)
-#TODO#1
-input_file = input_files[0] 
-input_filepath = os.path.join(input_dir, input_file)
+# List the input data files in directory and build filepath
+list_input_files = os.listdir(input_dir)
 
-# Read single .csv file into dataframe
-df = pd.read_csv(input_filepath, header = None)
+
+# Create empty list to store 
+list_dataframes = []
+
+for input_file in list_input_files:    
+    input_filepath = os.path.join(input_dir, input_file)    
+    list_dataframes.append(pd.read_csv(input_filepath, header = None))
+    
+    # Debugging: Convert datetime to string format
+    now = datetime.datetime.now()
+    date_time_now = now.strftime("%m/%d/%Y, %H:%M:%S")     
+    print("[COMPLETED " + date_time_now + "]: " + input_filepath)
+    
+df = pd.concat(list_dataframes)
 
 # Name columns of dataframe
 df.columns = ['TRANSACTION_ID',
@@ -49,19 +57,48 @@ df.columns = ['TRANSACTION_ID',
               'RECORD_STATUS' # A = Addition, C = Change, D = Delete
               ]
 
-# Load new files to new dataframe
-df_append = pd.read_csv(r'C:\Users\James.Bearne\Desktop\Python_HousePrice\data\pp-2019.csv', header = None)
-
-#TODO#2
-
-# Append two dataframes
-df = df.append(df_append, ignore_index=True)
-
 # Remove unintersting columns
-df = df[["TRANSFER_DATE", "PRICE", "PROPERTY_TYPE", "POAN", "STREET", "POSTCODE"]]
+df = df[["TRANSFER_DATE", "PRICE", "PROPERTY_TYPE", "POAN", "STREET", "POSTCODE", "RECORD_STATUS"]]
 
-# Set the postcode and select just those transactions
+# Set the postcode and select just those transactions into dataframe
+#TODO#4
 postcode = 'SW16 2BP'
 df_postcode = df.loc[df['POSTCODE'] == postcode]
+df_postcode = df_postcode.sort_values(by="TRANSFER_DATE", ascending = False)
 
-print(df_postcode.head())
+# Update the data type of TRANSFER_DATE to datetime
+df_postcode["TRANSFER_DATE"] = pd.to_datetime(df_postcode["TRANSFER_DATE"])
+
+#Add a new "TRANSFER_YEAR column to the dataframe
+df_postcode["TRANSFER_YEAR"] = df_postcode["TRANSFER_DATE"].dt.year
+
+#TODO#5
+
+# Set output data directory and filename
+output_dir = r'C:\Users\James.Bearne\Desktop\Python_HousePrice\data\output'
+output_file = 'output_postcode.csv'
+output_filepath = os.path.join(output_dir, output_file)
+
+# Save the selected transfers to csv file
+df_postcode.to_csv(path_or_buf = output_filepath, header = True, index = False)
+
+# Group by certain columns to get yearly mean average
+#TODO#3
+df_postcode_groupby = df_postcode.groupby(["TRANSFER_YEAR"]).mean()
+
+print(df_postcode_groupby)
+
+# Plot the price change
+df_postcode_groupby.plot.bar()
+
+
+
+
+
+
+
+
+
+
+
+
